6c82745b5473664b88d1e1c25d8dd550e1df5ceb
Sagar Sengupta <sagarsengupta.cse.tcea.2026@gmail.com>
2024-08-31 19:52:12 +0530

    adding some few changes

--- src/airline/management/system/DatabaseConnection.java
+++ src/airline/management/system/DatabaseConnection.java
@@ -14,30 +14,30 @@ public class DatabaseConnection {
 
     public DatabaseConnection() {
         try {
-            // Load the MySQL JDBC driver
+           
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Establish a connection to the database
+           
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/airlinemanagementsystem", "root", "Sagar@2003");
 
-            // Create a statement object for executing SQL queries
+           
             statement = connection.createStatement();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    // Getter for the connection object
+    
     public Connection getConnection() {
         return connection;
     }
 
-    // Getter for the statement object
+    
     public Statement getStatement() {
         return statement;
     }
 
-    // Close the connection and statement
+  
     public void close() {
         try {
             if (statement != null) {
